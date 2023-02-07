03ba49925372b793b9baec26ca5d9eac281a1a16
hanin77 <mohammadhanin77@gmail.com>
2023-02-07 21:30:14 +0000

    feat: add product-item component

---
+++ src/app/components/product-item/product-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/Product';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css'],
+})
+export class ProductItemComponent implements OnInit {
+  @Input() product: Product = {} as Product;
+
+  ngOnInit(): void {}
+}
