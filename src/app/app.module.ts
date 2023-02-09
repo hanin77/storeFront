657d44d5016a0112c7b26d9a79c56c936672a861
hanin77 <mohammadhanin77@gmail.com>
2023-02-09 22:10:34 +0000

    feat: add qte attribute to product-item component bind it to select-input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
@@ -16,7 +16,7 @@ import { ProductDetailsComponent } from './components/product-details/product-de
     ProductItemComponent,
     ProductDetailsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -8,6 +8,12 @@ import { Product } from 'src/app/models/Product';
 })
 export class ProductItemComponent implements OnInit {
   @Input() product: Product = {} as Product;
-
+  qte: number = 1;
   ngOnInit(): void {}
+  changeQte(event: any): void {
+    this.qte = event?.target?.value;
+  }
+  onSubmit(product: Product, event: Event): void {
+    console.log(product, this.qte);
+  }
 }
