88b1fd5b38fbfea284bf67aa58cb561baeb2e4d0
hanin77 <mohammadhanin77@gmail.com>
2023-02-10 18:27:42 +0000

    feat:bind cart service to product-details & product-item action button

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/Product';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,9 +12,11 @@ import { Product } from 'src/app/models/Product';
 export class ProductDetailsComponent implements OnInit {
   id: number | undefined;
   product: Product | undefined = undefined;
+  qte: number = 1;
   constructor(
     private route: ActivatedRoute,
-    private productService: ProductService
+    private productService: ProductService,
+    private cartService: CartService
   ) {}
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
@@ -23,7 +26,12 @@ export class ProductDetailsComponent implements OnInit {
       if (this.id) {
         this.product = res.find((pr) => pr.id === this.id);
       }
-      console.log(this.product);
     });
+  }
+  changeQte(event: any): void {
+    this.qte = event?.target?.value;
+  }
+  onSubmit(product: Product | undefined): void {
+    this.cartService.addItemToCart({ ...(product as Product), qte: this.qte });
   }
 }
--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/Product';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-product-item',
@@ -9,11 +10,12 @@ import { Product } from 'src/app/models/Product';
 export class ProductItemComponent implements OnInit {
   @Input() product: Product = {} as Product;
   qte: number = 1;
+  constructor(private cartService: CartService) {}
   ngOnInit(): void {}
   changeQte(event: any): void {
     this.qte = event?.target?.value;
   }
-  onSubmit(product: Product, event: Event): void {
-    console.log(product, this.qte);
+  onSubmit(product: Product): void {
+    this.cartService.addItemToCart({ ...product, qte: this.qte });
   }
 }
