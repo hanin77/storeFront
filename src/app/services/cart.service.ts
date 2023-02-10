468fb0a482c24356be6d9e5a644b90d1669a6651
hanin77 <mohammadhanin77@gmail.com>
2023-02-10 21:38:59 +0000

    feat: handle alert and keep initial cart items orderwhen updating item qte inside addItemTOCart method

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -12,10 +12,28 @@ export class CartService {
     return this.cart;
   }
   addItemToCart(cartItem: CartItem) {
-    this.cart = [
-      ...this.cart.filter((item) => item.id !== cartItem.id),
-      cartItem,
-    ];
+    if (cartItem.qte === 0) {
+      alert('item rmoved from cart!');
+      this.cart = [...this.cart.filter((item) => item.id !== cartItem.id)];
+    } else {
+      //if new item added to top of cart
+      if (!this.cart.find((item) => item.id == cartItem.id)) {
+        alert('added to cart!');
+        this.cart = [
+          ...this.cart.filter((item) => item.id !== cartItem.id),
+          cartItem,
+        ];
+      } else {
+        const itemIndex = this.cart
+          .map((i) => {
+            return i.id;
+          })
+          .indexOf(cartItem.id);
+        console.log(itemIndex);
+        this.cart.splice(itemIndex, 1, cartItem);
+      }
+    }
+
     return this.cart;
   }
 }
