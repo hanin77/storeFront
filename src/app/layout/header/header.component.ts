09cc6b7b235e95da6fdedf570b901733c6cbc250
hanin77 <mohammadhanin77@gmail.com>
2023-02-12 00:23:26 +0000

    feat:add cart icon

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  faCartArrowDown = faCartArrowDown;
 }
